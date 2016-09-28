f0507c33fe36ec5d90b8c1a3398b6d17c452c0ae
George Zhu <zyqgeorge@gmail.com>
2016-09-28 12:21:14 -0600

    fixed shuffle

--- Card Game/src/Shuffle.java
+++ Card Game/src/Shuffle.java
@@ -1,8 +1,17 @@
-
+import java.util.Collections;
 public class Shuffle
 	{
 		public static void shuffle(){
 			Setdeck.generateDeck();
-			
-		}		
+			Collections.shuffle(Setdeck.deck);		
+
+		}	
+//		for(int i = 0; i < 52; i++)
+//			{
+//				System.out.println(i + " = " + Setdeck.deck.get(i).getFace() + " of " + Setdeck.deck.get(i).getSuit() + " rank = " + Setdeck.deck.get(i).getRank());
+//			}
+//		for (Card c : Setdeck.deck)
+//		{
+//			System.out.println(c.getRank());
+//		}
 	}
